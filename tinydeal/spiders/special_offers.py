f95a705efc8f2bea0ecc833d1b46397eb2b56d31
Oussema Sferi <oussema.sferi@gmail.com>
2022-09-17 18:37:59 +0100

    changing request headers - User-Agent

--- tinydeal/spiders/special_offers.py
+++ tinydeal/spiders/special_offers.py
@@ -4,16 +4,28 @@ import scrapy
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
     allowed_domains = ['web.archive.org']
-    start_urls = ['https://web.archive.org/web/20161226163139/https://www.tinydeal.com/specials.html']
+
+    # start_urls = ['https://web.archive.org/web/20161226163139/https://www.tinydeal.com/specials.html']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://web.archive.org/web/20161226163139/https://www.tinydeal.com/specials.html',
+                             callback=self.parse, headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+            })
 
     def parse(self, response):
         for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
             yield {
                 'title': product.xpath(".//a[@class='p_box_title']/text()").get(),
                 'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
-                'discounted_price': product.xpath(".//div[@class='p_box_price']/span[@class='productSpecialPrice fl']/text()").get(),
-                'original_price': product.xpath(".//div[@class='p_box_price']/span[@class='normalprice fl']/text()").get()
+                'discounted_price': product.xpath(
+                    ".//div[@class='p_box_price']/span[@class='productSpecialPrice fl']/text()").get(),
+                'original_price': product.xpath(
+                    ".//div[@class='p_box_price']/span[@class='normalprice fl']/text()").get(),
+                'User-Agent': response.request.headers['User-Agent']
             }
         next_page = response.xpath("//a[@class='nextPage']/@href").get()
         if next_page:
-            yield scrapy.Request(url=next_page, callback=self.parse)
+            yield scrapy.Request(url=next_page, callback=self.parse, headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+            })
